a172f9f8c28a2da159fa914599d336dfbb054031
Daniel Degirmen <daniel.degirmen@sgu.se>
2021-10-28 10:34:47 +0200

    Initial commit

---
+++ src/test/java/guru/springframework/sfgrecipeapp/SfgRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
