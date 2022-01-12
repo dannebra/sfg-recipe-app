b64e194bde0336fab9ffa50f620ce5e1537bc1da
Daniel Degirmen <daniel.degirmen@sgu.se>
2022-01-12 14:38:08 +0100

    Added expectation for model attribute to test

--- src/test/java/guru/springframework/sfgrecipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/sfgrecipeapp/controllers/RecipeControllerTest.java
@@ -13,8 +13,7 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class RecipeControllerTest {
 
@@ -41,7 +40,8 @@ class RecipeControllerTest {
 
         mockMvc.perform(get("/recipe/show/1"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("recipe/show"));
+                .andExpect(view().name("recipe/show"))
+                .andExpect(model().attributeExists("recipe"));
     }
 
     @Test
